441d07501121c65a11a30feeeb9691ca330380e0
Yash Mittal <mittal.yash12@hotmail.com>
2022-10-21 09:59:20 +0530

    Fix: Fix BUgs
    Signed-off-by: Yash Mittal <mittal.yash12@hotmail.com>

--- apps/bap/src/main.ts
+++ apps/bap/src/main.ts
@@ -3,6 +3,10 @@ import { BapModule } from './bap.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(BapModule);
+  app.enableCors({
+    origin: 'http://localhost:5173',
+    methods: 'POST',
+  });
   await app.listen(3000);
 }
 bootstrap();
